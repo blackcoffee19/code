de02cee284886d71b6e6c6093d8579a6fdc20141
blackcoffee19 <cattuongw2000@gmail.com>
2022-05-02 00:40:04 +0700

    2-5 first commit

--- C#/class1/Class1.cs
+++ C#/class1/Class1.cs
@@ -6,9 +6,28 @@ namespace Class
     {
         static void Main(string[] args)
         {
-            Bank myacc1 = new Bank("ja");
-            Console.WriteLine(myacc1.Name);
-            Console.WriteLine(myacc1.id);
+            Bank.Greeting();
+            bool startCreate = true;
+            while(startCreate){
+                Console.Write("Enter your name: (type exit to exit) ");
+                string? inputName = Console.ReadLine();
+                if(inputName == "exit"){
+                    startCreate = false;
+                } else if(inputName == null){
+                    continue;
+                }else{
+                    Console.Write("Enter your ID (if you dont have any ID type 1): ");
+                    string? inputID = Console.ReadLine();
+                    while(inputID == null || inputID.Length < 6 && inputID != "1") {
+                        Console.Write("Enter your ID (if you dont have any ID type 1): ");
+                        inputID = Console.ReadLine();
+                    };
+                    int converID = Int32.Parse(inputID);
+                    Bank acc = new Bank(inputName, converID);
+                }
+            };
+            int totalAcc = Bank.TotalAcc;
+            Console.WriteLine(totalAcc);
         }
     }
 }
--- C#/class1/Class2.cs
+++ C#/class1/Class2.cs
@@ -4,16 +4,17 @@ namespace Class
 {
     public class Bank {
         public int account;
-        private string country;
         private int age;
         private int ID;
         private int[] IDs = new int[] {213184,231476,364336,866333};
-        //private int Index = 4;
+        static int count;
+
 
 
         //CONSTRUCTORS
         public Bank(string client, int id) {
             this.Name= client;
+            CheckID(id);
             this.ID = id; 
         }
         public Bank(string client) : this(client, 0) {
@@ -27,41 +28,64 @@ namespace Class
         }
         public int id {
             get { return ID;}
-            set {
-                if(IDs.Contains(id)) {
-                    Console.WriteLine("Account available");
-                    if(id == IDs[0] || id == IDs[1]) {
-                        account = 1000000;
-                        Console.WriteLine($"Your account: {account}VND");
-                    } else if(id == IDs[2] || id == IDs[3]){
-                        account = 30000;
-                        Console.WriteLine($"Your account: {account}VND");
-                    }
-                } else {
-                    Console.WriteLine("No acceptable!");
-                }
-            }
         }
         //METHOD
+        public void CheckID(int id) {
+                if(IDs.Contains(id)) {
+                    Console.WriteLine("Account available");
+                    count++;
+                    account = own(); 
+                    Console.WriteLine($"Your account: {account}VND");
+                } else {
+                    Console.Write("No acceptable!\n Do you want to create an account? (yes/no) ");
+                    string? solu = Console.ReadLine();
+                    if(solu == null || solu == "no") {
+                        Console.WriteLine("Good bye!");
+                    } else {
+                        createAccount();
+                    }
+
+                }
+        }
         public void getInfo(){
             Console.WriteLine("Name: " + Name);
         }
         public void createAccount() {
             Console.WriteLine("Enter your country: ");
-            string inputCountry = Console.ReadLine();
-            this.country = inputCountry;
+            string? inputCountry = Console.ReadLine();
+            while(inputCountry == null){
+            Console.WriteLine("Enter your country: ");
+            inputCountry = Console.ReadLine();    
+            };
             Console.WriteLine("Enter your age: ");
-            string inputAge = Console.ReadLine();
+            string? inputAge = Console.ReadLine();
+            while(inputAge == null){
+                Console.WriteLine("Enter your age: ");
+                inputAge = Console.ReadLine();
+            };
             this.age = Int32.Parse(inputAge);
             account = 0;
             int id = getID();
             this.ID = id;
+            //IDs[IDs.Length] = id;
             Console.WriteLine($"Name: {Name}\nAge: {age}\nID: {ID}\nYour balance: {account}\n");
+            count++;
         }
         public int getID(){
             Random ranID = new Random();
             return ranID.Next(1000000); 
         }
+        public int own() {
+            Random ranMoney = new Random();
+            return ranMoney.Next(10000,1000000000);
+        }
 
+        //STATIC
+        public static int TotalAcc {
+            get {return count;}
+        }
+        public static void Greeting() {
+            Console.WriteLine("*-----* Welcome to Bank *-----*");
+        }
     }      
 }   
